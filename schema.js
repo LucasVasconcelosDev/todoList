aa1be363441da8fcb21d53e6dee8c20ae393812f
Daniel Medrado <d_medrado@yahoo.com>
2024-05-02 17:41:28 -0300

    Update schema.js

--- schema.js
+++ schema.js
@@ -3,17 +3,17 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://lucasvasconcelos:wuwg5ypL3W1uEL6G@cluster0.ay6siya.mongodb.net/");
 
 const schemaTodoList = new mongoose.Schema({
-    titulo:    {type: String, required: true},
-    descricao: {type: String, required: true},
-    categoria: {type: String, required: true},
-    datahora:  {type: Date, required: true},
+    title:    {type: String, required: true},
+    description: {type: String, required: true},
+    category: {type: String, required: true},
+    date:  {type: Date, required: true},
     idUsuario: {type: String, required: true}
 });
 
 const schemaUsuarios = new mongoose.Schema({
     username:  {type: String, required: true, unique: true},
     email:     {type: String, required: true, unique: true},
-    senha:     {type: String, required: true},
+    password:     {type: String, required: true},
     foto:      {type: String, required: true}
 });
 
