a4b96028ea8f379c47bc25941670a8ed6d93ab79
Daniel Medrado <d_medrado@yahoo.com>
2024-06-12 18:11:18 -0300

    Add files via upload

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './styles/toastCustomStyle.css';
 import Rotas from './routes/index';
 
 function App() {
-
   return (
-
     <div>
       <Rotas />
       <ToastContainer
         position="top-center"
         autoClose={3000}
-        style={{ width: '500px' }}
+        toastClassName="custom-toast-container"
       />
     </div>
   );
